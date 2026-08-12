cb91be46bcc167a3287e2d944b6e3a2b1a9cf396
taurus <attemptorgoo@gmail.com>
2026-08-12 12:46:32 +0800

    regFile自检程序

--- vFile/verilog/verilog/test.sv
+++ vFile/verilog/verilog/test.sv
@@ -1,19 +1,33 @@
 module test();
 logic clk,rst;
 
-initial begin
-    rst = 1'b0;
-    clk = 1'b0;
-
-    // #30 rst = 1'b0;   避免高阻态x的出现
-    #50 rst = 1'b1;
-    #500 $finish;
-end
-
-always #10 clk = ~clk;
-
 cpu u_cpu (
     .clk(clk),
     .rst(rst) // 确保你的 cpu.sv 里的复位逻辑和这里对应
 );
+
+task check_regFile(
+input int unsigned index
+input logic [31:0] expected_value
+);
+    begin
+        if (u_cpu.myID.myRf.regs[index] !== expected_value) begin
+            $error("Test failed: Register x%0d expected %h, got %h", index, expected_value, u_cpu.myID.myRf.regs[index]);
+        end else begin
+            $display("Test passed: Register x%0d has value %h", index, u_cpu.myID.myRf.regs[index]);
+        end
+    end
+endtask
+
+always #5 clk = ~clk; // 10ns clock period
+initial begin
+    rst = 1'b0;
+    clk = 1'b0;
+
+    #50 rst = 1'b1;
+    #500 
+    check_regFile(1, 32'h00000005); // 检查寄存器 x1 是否为 5
+    $finish;
+end
+
 endmodule
